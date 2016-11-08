df93581926facea40bcf6cceb9dad740e592694a
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2016-11-08 17:45:17 -0500

    adds paperclip, aws and attachments to titles

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates_presence_of :title, :text
+
+  ATTACHMENTS = %w(
+    image/jpeg image/jpg image/png application/pdf application/msword
+    application/vnd.openxmlformats-officedocument.wordprocessingml.attachment
+  ).map(&:freeze).freeze
+  has_attached_file :attachment
+  validates_attachment :attachment, content_type: { content_type: ATTACHMENTS }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108200015) do
+ActiveRecord::Schema.define(version: 20161108221819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,9 +65,13 @@ ActiveRecord::Schema.define(version: 20161108200015) do
     t.string   "slug"
     t.text     "text"
     t.boolean  "private"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.integer  "user_id"
+    t.string   "attachment_file_name"
+    t.string   "attachment_content_type"
+    t.integer  "attachment_file_size"
+    t.datetime "attachment_updated_at"
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
