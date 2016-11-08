5484cdfd765cf0ab5a94ed70be102eecd9496e7d
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2016-11-08 10:26:26 -0500

    adds country to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable
+
+  def country_name
+    country = self.country
+    ISO3166::Country[country]
+  end
 end
