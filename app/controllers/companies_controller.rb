087bd5e0588d4964cb02fcafd4ce3fb3c407cb93
Andrei Motinga <andrei@Andreis-MacBook-Air.local>
2016-11-10 09:58:54 -0500

    clean up companies ctrl

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,42 +1,7 @@
-# todo clean up
 class CompaniesController < ApplicationController
-  before_action :set_company, only: [:show, :edit, :update, :destroy, :public,
-                                     :private]
+  before_action :set_company, only: [:show, :public, :private]
 
-  def index
-    @companies = Company.all
-  end
-
-  # GET /companies/1
   def show
-  end
-
-  # GET /companies/new
-  def new
-    @company = Company.new
-  end
-
-  # GET /companies/1/edit
-  def edit
-  end
-
-  # POST /companies
-  def create
-    @company = Company.new(company_params)
-
-    if @company.save
-      redirect_to @company, notice: 'Company was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  # PATCH/PUT /companies/1
-
-  # DELETE /companies/1
-  def destroy
-    @company.destroy
-    redirect_to companies_url, notice: 'Company was successfully destroyed.'
   end
 
   def public
@@ -52,10 +17,8 @@ class CompaniesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_company
-      @company = Company.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
+  def set_company
+    @company = Company.find(params[:id])
+  end
 end
